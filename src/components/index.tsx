3ab32d3adc2eeb7e3d7ca3023672173a18546f9e
mikeleo03 <leonmichael463@gmail.com>
2025-05-30 18:13:35 +0700

    [Feat] New frontend component from ShadcnUI

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import { Input }  from "./ui/input";
 import { Button } from './ui/button';
 import { Card } from './ui/card';
 import { Label } from './ui/label';
+import { Switch } from "./ui/switch";
 
-export { Navbar, Footer, Input, Button, Card, Label };
+export { Navbar, Footer, Input, Button, Card, Label, Switch };
